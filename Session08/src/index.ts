1ecb90012148ad47f88cfdf8c017ef3ad0da53a1
PinkD <luongdat114zu@gmail.com>
2025-08-15 19:29:12 +0700

    index.ts

--- Session08/src/index.ts
+++ Session08/src/index.ts
@@ -53,3 +53,6 @@ let user2: Omit<User, "salary"> = {
     email:"cuccuc@gmail.com",
     address:"hn"
 }
+//function findItemById<T extends { id: number }>(items: T[], id: number): T | undefined {
+//     return items.find(item => item.id === id);
+// }
